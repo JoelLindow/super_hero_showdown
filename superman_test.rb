a759eae79fd29f3db565296565c42f2b2c044c98
JoelLindow <joellindow@gmail.com>
2017-06-19 17:19:02 -0600

    add superman exercise

--- superman_test.rb
+++ superman_test.rb
@@ -1,7 +1,106 @@
-# ARRAY EXERCISE SOON TO BE WRITTEN
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative 'superman'
 
-# superman exists
-# secret identity is clark kent (read instance variable, but instance variable is not attr_reader.)
-# has an empty array called "jailed_enemies"
-# use count message to check how many are in jail
-# make method to put bad guys in jail
+class SupermanTest < Minitest::Test
+
+  def test_it_exists
+    hero = Superman.new("Superman", "Clark Kent")
+
+    assert_instance_of Superman, hero
+  end
+
+  def test_it_has_a_secret_identity
+    hero = Superman.new("Superman", "Clark Kent")
+
+    assert_equal "Superman", hero.name
+    assert_equal "Clark Kent", hero.secret_identity
+  end
+
+  def test_superman_has_access_to_the_jail
+    hero = Superman.new("Superman", "Clark Kent")
+
+    assert_equal [], hero.jailed_enemies
+  end
+
+  def test_superman_can_send_bad_guys_to_jail
+    hero = Superman.new("Superman", "Clark Kent")
+    hero.send_enemy_to_jail("Lex Luthor")
+
+    assert_equal ["Lex Luthor"], hero.jailed_enemies
+  end
+
+  def test_superman_knows_how_many_enemies_are_in_jail
+    hero = Superman.new("Superman", "Clark Kent")
+
+    assert_equal 0, hero.how_many_enemies_in_jail?
+
+    hero.send_enemy_to_jail("Lex Luthor")
+
+    assert_equal 1, hero.how_many_enemies_in_jail?
+
+    hero.send_enemy_to_jail("General Zod")
+    hero.send_enemy_to_jail("Bizzaro")
+
+    assert_equal 3, hero.how_many_enemies_in_jail?
+    assert_equal ["Lex Luthor", "General Zod", "Bizzaro"], hero.jailed_enemies
+  end
+
+  def test_superman_can_get_an_alphebetized_list_of_jailed_enemies
+    hero = Superman.new("Superman", "Clark Kent")
+    hero.send_enemy_to_jail("Lex Luthor")
+    hero.send_enemy_to_jail("General Zod")
+    hero.send_enemy_to_jail("Bizzaro")
+    hero.send_enemy_to_jail("Mister Mxyzptlk")
+
+    assert_equal(
+                ["Lex Luthor", "General Zod", "Bizzaro", "Mister Mxyzptlk"],
+                hero.jailed_enemies
+                )
+    assert_equal(
+                ["Bizzaro", "General Zod", "Lex Luthor", "Mister Mxyzptlk"],
+                hero.enemies_in_jail
+                )
+  end
+
+  def test_superman_can_release_oldest_jailed_enemy
+    hero = Superman.new("Superman", "Clark Kent")
+    hero.send_enemy_to_jail("Lex Luthor")
+    hero.send_enemy_to_jail("General Zod")
+    hero.send_enemy_to_jail("Bizzaro")
+    hero.send_enemy_to_jail("Mister Mxyzptlk")
+
+    assert_equal(
+                ["Lex Luthor", "General Zod", "Bizzaro", "Mister Mxyzptlk"],
+                hero.jailed_enemies
+                )
+
+    hero.release_oldest_prisoner
+
+    assert_equal(
+                ["General Zod", "Bizzaro", "Mister Mxyzptlk"],
+                hero.jailed_enemies
+                )
+  end
+
+  def test_superman_can_release_newest_jailed_enemy
+    hero = Superman.new("Superman", "Clark Kent")
+    hero.send_enemy_to_jail("Lex Luthor")
+    hero.send_enemy_to_jail("General Zod")
+    hero.send_enemy_to_jail("Bizzaro")
+    hero.send_enemy_to_jail("Mister Mxyzptlk")
+
+    assert_equal(
+                ["Lex Luthor", "General Zod", "Bizzaro", "Mister Mxyzptlk"],
+                hero.jailed_enemies
+                )
+
+    hero.release_newest_prisoner
+
+    assert_equal(
+                ["Lex Luthor", "General Zod", "Bizzaro"],
+                hero.jailed_enemies
+                )
+  end
+end
